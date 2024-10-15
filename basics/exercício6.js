f287d021b030c03ae2f4dc58a1409ef6756532b8
Miguel Ectil <2301985@SALAS.AULAS>
2024-10-15 20:33:37 -0300

    [ADD] Adicionando exercício 5

---
+++ basics/exercício6.js
@@ -0,0 +1,8 @@
+// ==== Encontrar Antecessor e Sucessor ==== 
+
+let num1 = 9
+const antece = num1 - 1
+const sucess = num1 + 1
+console.log(`Digite um número = ${num1}`)
+console.log(`O antecessor de ${num1} é ${antece}`)
+console.log(`O sucessor de ${num1} é ${sucess}`)
