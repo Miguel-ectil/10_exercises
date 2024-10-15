3708dffe03e378c67dac34b87c3d4f9254e2ba65
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-15 17:58:53 -0300

    [ADD] Adicionando código do exercício 1 apresentação boas vindas

--- basics/exercício1.js
+++ basics/exercício1.js
@@ -1,1 +1,19 @@
-// Exercício ----
+// Exercício ---- leer nome e mostrar mensagem de boas vindas ----
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Qual é o seu nome? ', (answer) => {
+  console.log(`Olá ${answer}, é um prazer te conhecer!`);
+  rl.close();
+});
+
+
+
+// Outra forma de fazer com input
+let nome = "Miguel Ectil"
+console.log(`Olá ${nome}, é um prazer te conhecer!`);
