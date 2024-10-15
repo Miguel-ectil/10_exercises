8e96b712183041e231ae76d63d55f9ab7d7b12d4
Miguel Ectil <2301985@SALAS.AULAS>
2024-10-15 20:23:19 -0300

    [ADD] Adicionando exercício4 'Mostrando soma entre dois valores'

---
+++ basics/exercício4.js
@@ -0,0 +1,9 @@
+// Exercício ---- Mostrando soma ----
+
+
+let num1 = 8
+let num2 = 5
+const soma = num1 + num2
+console.log(`Digite um valor = ${num1}`)
+console.log(`Digite outro valor = ${num2}`)
+console.log(`A soma entre ${num1} e ${num2} é igual a ${soma}`)
