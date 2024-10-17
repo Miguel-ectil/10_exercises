ef6922ab42b64cbcb096b732cc3ec145842b4987
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-17 17:52:40 -0300

    [ADD] Adicionando

---
+++ basics/exercício7.js
@@ -0,0 +1,7 @@
+console.log("Digite um valor real: ");
+let valor = 3.5
+const dobro = valor * 2
+tercaParte = valor / 3
+
+console.log(`O dobro de ${valor} `, dobro)
+console.log(`A terça parte de ${valor}`, tercaParte.toFixed(5));
